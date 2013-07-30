d266f72ec5a2aa11996ea30fdd18abd335378d27
fasseg <frank.asseg@googlemail.com>
2013-07-30 18:09:59 +0200

    added license header and attribution

--- src/main/java/eu/scape_project/service/ConnectorService.java
+++ src/main/java/eu/scape_project/service/ConnectorService.java
@@ -1,6 +1,17 @@
-/**
- *
+/*
+   Licensed under the Apache License, Version 2.0 (the "License");
+   you may not use this file except in compliance with the License.
+   You may obtain a copy of the License at
+
+       http://www.apache.org/licenses/LICENSE-2.0
+
+   Unless required by applicable law or agreed to in writing, software
+   distributed under the License is distributed on an "AS IS" BASIS,
+   WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+   See the License for the specific language governing permissions and
+   limitations under the License.
  */
+
 
 package eu.scape_project.service;
 
@@ -73,6 +84,12 @@ import gov.loc.marc21.slim.RecordType;
 import gov.loc.mix.v20.Mix;
 import gov.loc.videomd.VideoType;
 
+/**
+ * Component which does all the interaction with fcrepo4
+ *
+ * @author frank asseg
+ *
+ */
 @Component
 public class ConnectorService {
 
